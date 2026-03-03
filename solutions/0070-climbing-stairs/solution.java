e96f367dbfe1d324245d8a0ffc9cfad9ff3a9ae6
luigi <luigicussigh59@gmail.com>
2026-03-03 07:33:38 +0000

    [LeetCode sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (96.43%)

---
+++ solutions/0070-climbing-stairs/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int[] cache = new int[100];
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            if (cache[n] == 0) {
+                cache[n] = climbStairs(n - 1) + climbStairs(n - 2);
+            }
+            return cache[n];
+        }
+    }
+}
