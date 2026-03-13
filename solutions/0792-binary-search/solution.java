46ec89506a6421ae5a993ecde83ea9124b3292b0
luigi <luigicussigh59@gmail.com>
2026-03-13 10:07:12 +0000

    [LeetCode sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (100.00%)

---
+++ solutions/0792-binary-search/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (target > nums[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        System.gc();
+
+        return nums[l] == target ? l : -1;
+    }
+}
