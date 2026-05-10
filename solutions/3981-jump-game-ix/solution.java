eaf128d45f5ee7079cf35a6d6d1ba4ab43ee2475
luigi <luigicussigh59@gmail.com>
2026-05-10 10:41:47 +0000

    [LeetCode sync] Runtime - 4 ms (99.99%), Memory - 197.5 MB (84.03%)

--- solutions/3981-jump-game-ix/solution.java
+++ solutions/3981-jump-game-ix/solution.java
@@ -1,32 +1,26 @@
 class Solution {
-
-    record Item(int value, int left, int right) {}
-
     public int[] maxValue(int[] nums) {
         int n = nums.length;
+
         int[] ans = new int[n];
+        int[] prefix = new int[n];
 
-        List<Item> stack = new ArrayList<>();
+        prefix[0] = nums[0];
 
-        for (int i = 0; i < n; i++) {
-            Item curr = new Item(nums[i], i, i);
-
-            while (!stack.isEmpty() && stack.getLast().value() > nums[i]) {
-                Item top = stack.removeLast();
-                curr = new Item(
-                    Math.max(curr.value(), top.value()),
-                    top.left(),
-                    curr.right()
-                );
-            }
-
-            stack.add(curr);
+        for (int i = 1; i < n; i++) {
+            prefix[i] = Math.max(prefix[i - 1], nums[i]);
         }
 
-        for (int i = 0; i < stack.size(); i++) {
-            for (int j = stack.get(i).left(); j <= stack.get(i).right(); j++) {
-                ans[j] = stack.get(i).value();
-            }
+        int suffix = Integer.MAX_VALUE;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (prefix[i] > suffix)
+                ans[i] = ans[i + 1];
+            else
+                ans[i] = prefix[i];
+
+            suffix = Math.min(suffix, nums[i]);
         }
 
         return ans;
